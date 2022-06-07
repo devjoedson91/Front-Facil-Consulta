d6314390d6fe41d90504053b4aa3d03e388d481c
Joedson Ferreira <joedson.devel@gmail.com>
2022-06-07 13:14:04 -0300

    Update revisao.vue

--- src/views/Revisao.vue
+++ src/views/Revisao.vue
@@ -48,7 +48,7 @@
                 <div class="mb-3">
                     <label for="cpf" class="fw-bold">Forma de pagamento da consulta</label>
                     <div class="input-group flex-nowrap">
-                        <div class="fs-6">{{formsPgto}} {{condicoes}}</div>
+                        <div class="fs-6">{{formsPgto}}{{condicoes}}</div>
                     </div>
                 </div>
 
@@ -112,8 +112,8 @@
                 this.cidade = cidade;
                 this.especialidade = especialidade;
                 this.valor = valorConsulta;
-                this.formsPgto = formsPgto;
-                this.condicoes = condicoes;
+                this.formsPgto = formsPgto.replace('  ', ' - ');
+                this.condicoes = condicoes !== '' ? `- ${condicoes}` : '';
 
             },
 
